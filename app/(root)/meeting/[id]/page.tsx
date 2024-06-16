ac4a012a261222ad2154bfc61ceb1e728711a65c
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-06-16 07:08:32 +0600

    refactor: Add lazy loading for sidebar images and create meeting functionality

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+"use client";
 
-export default function Meeting({ params }: { params: { id: string } }) {
-  return <div>Meeting Room: #{params.id}</div>;
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+
+export default function Meeting({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { calls, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={calls}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 }
---
+++ hooks/useGetCallById.ts
@@ -0,0 +1,28 @@
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+
+export function useGetCallById(id: string | string[]) {
+  const [calls, setCalls] = useState<Call>();
+  const [isCallLoading, setIsCallLoading] = useState(true);
+
+  const client = useStreamVideoClient();
+
+  useEffect(() => {
+    if (!client) return;
+
+    const loadCall = async () => {
+      const { calls } = await client.queryCalls({
+        filter_conditions: {
+          id,
+        },
+      });
+
+      if (calls.length > 0) setCalls(calls[0]);
+
+      setIsCallLoading(false);
+    };
+    loadCall();
+  }, [client, id]);
+
+  return { calls, isCallLoading };
+}
