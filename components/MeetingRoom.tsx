3c4f03dabf03e45e30808882ffa1e59eac1cc188
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-06-16 22:36:47 +0600

    refactor: Add lazy loading for sidebar images in MeetingRoom component

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,3 +1,46 @@
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import { useState } from "react";
+
 export default function MeetingRoom() {
-  return <div>MeetingRoom</div>;
+  const [layout, setLayout] = useState("speaker-left");
+  const [showParticipant, setShowParticipant] = useState(false);
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
+
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+
+        <div
+          className={cn("ml-2 hidden h-[calc(100vh-86px)]", {
+            "show-block": showParticipant,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipant(false)} />
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
+      </div>
+    </section>
+  );
 }
