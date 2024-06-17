7426ad86f988b91de42052d02c30e3a8cbfe7e4b
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-06-18 03:57:14 +0600

    refactor: Add lazy loading for sidebar images in Upcoming page and update avatarImages constant

---
+++ components/CallList.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import useGetCalls from "@/hooks/useGetCalls";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Loader from "./Loader";
+import MeetingCard from "./MeetingCard";
+
+type CallListProps = {
+  type: "ended" | "upcoming" | "recordings";
+};
+
+export default function CallList({ type }: CallListProps) {
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const router = useRouter();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "upcoming":
+        return upcomingCalls;
+      case "recordings":
+        return recordings;
+      default:
+        return [];
+    }
+  };
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No Previous Calls";
+      case "upcoming":
+        return "No Upcoming Calls";
+      case "recordings":
+        return "No Recordings";
+      default:
+        return "";
+    }
+  };
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+            key={(meeting as Call).id}
+            icon={
+              type === "ended"
+                ? "/icons/previous.svg"
+                : type === "upcoming"
+                  ? "/icons/upcoming.svg"
+                  : "/icons/recording.svg"
+            }
+            title={
+              (meeting as Call).state.custom.description.substring(0, 25) ||
+              "No description"
+            }
+            date={
+              (meeting as Call).state.startsAt?.toLocaleString() ||
+              (meeting as CallRecording).start_time.toLocaleString()
+            }
+            isPreviousMeeting={type === "ended"}
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`${(meeting as CallRecording).url}`)
+                : () => router.push(`/meeting/${(meeting as Call).id}`)
+            }
+            link={
+              type === "recordings"
+                ? (meeting as CallRecording).url
+                : `${window.location.origin}/call/${(meeting as Call).id}`
+            }
+            buttonText={type === "recordings" ? "Play" : "Start Meeting"}
+          />
+        ))
+      ) : (
+        <h1>{noCallsMessage}</h1>
+      )}
+    </div>
+  );
+}
